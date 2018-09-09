2eccfe19895aab3aa029a1574637cb04b6b78073
marcincholody <43113852+marcincholody@users.noreply.github.com>
2018-09-09 17:14:06 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Wisniowa Time</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript" src="skrypt.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+</head>
+<body onload="start();">
+		<center>
+		<div id="main">
+				
+
+
+		</div>
+		</center>
+
+</body>
+</html>
